cf855e34c0767d6afa3f9d92031c3832898a7734
danielVFS <danielv759153@gmail.com>
2020-06-23 18:25:28 -0300

    update user

--- Backend/src/app/controllers/UserController.js
+++ Backend/src/app/controllers/UserController.js
@@ -16,7 +16,29 @@ class UserController {
   }
 
   async update(req, res) {
-    return res.json({ ok: true });
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    // if the user is changing email
+    if (email !== user.email) {
+      const userExists = await User.findOne({
+        where: { email },
+      });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'email already exists' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    // Everything is ok
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({ id, name, email, provider });
   }
 }
 
