258a3bfb3831d71f06a443f1a9207949b27613a6
danielVFS <danielv759153@gmail.com>
2020-06-23 15:06:57 -0300

    user model and loader models

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'hello' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'daniel',
+    email: 'daniel@gmail.com',
+    password_hash: '22525',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
