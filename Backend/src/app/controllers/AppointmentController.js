c37768ae39aabef1ca431b5d895f6b443f3641d6
danielVFS <danielv759153@gmail.com>
2020-06-24 15:33:50 -0300

    create appointment

---
+++ Backend/src/app/controllers/AppointmentController.js
@@ -0,0 +1,55 @@
+import * as Yup from 'yup';
+
+import User from '../models/User';
+import Appointment from '../models/Appointment';
+
+class AppointmentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+      date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { provider_id, date } = req.body;
+
+    /**
+     * Check if provider_id is a provider
+     */
+    const checkIsProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+
+    if (!checkIsProvider)
+      return res
+        .status(401)
+        .json({ error: 'You can only create appointments with providers' });
+
+    /**
+     * checking if the provider is scheduling with yourself
+     */
+    const providerNotSchedule = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (providerNotSchedule) {
+      return res
+        .status(401)
+        .json({ error: 'you cannot schedule with yourself' });
+    }
+
+    // Everything is OK
+    const appointment = await Appointment.create({
+      user_id: req.userId,
+      provider_id,
+      date,
+    });
+
+    return res.json(appointment);
+  }
+}
+
+export default new AppointmentController();
