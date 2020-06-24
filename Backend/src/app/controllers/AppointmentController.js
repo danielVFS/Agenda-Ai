518551c3bc36fb9eaee689ca83dfc0eafe41e31c
danielVFS <danielv759153@gmail.com>
2020-06-24 16:11:14 -0300

    validating appointments with date-fns

--- Backend/src/app/controllers/AppointmentController.js
+++ Backend/src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 
 import User from '../models/User';
 import Appointment from '../models/Appointment';
@@ -39,6 +40,32 @@ class AppointmentController {
       return res
         .status(401)
         .json({ error: 'you cannot schedule with yourself' });
+    }
+
+    /**
+     * Check for past dates
+     */
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'past dates are not permitted' });
+    }
+
+    /**
+     * Check date availability
+     */
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not availability' });
     }
 
     // Everything is OK
