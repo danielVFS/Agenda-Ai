8621d2252b0b6bb62b78487702eefcb8c9893bb3
danielVFS <danielv759153@gmail.com>
2020-06-26 13:58:22 -0300

    sending email with nodemailer

---
+++ Backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '97b01c9f492440',
+    pass: '023ce89487b6d1',
+  },
+  default: {
+    from: 'Equipe AgendaAí <noreplayagendaai.com>',
+  },
+};
