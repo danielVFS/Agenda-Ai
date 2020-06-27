c19a594ac7d89b88a356724c3a82f5288161674e
danielVFS <danielv759153@gmail.com>
2020-06-27 14:23:16 -0300

    cancelable and past fields por model Appointment adn add sentry for track errors in application

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,19 +1,25 @@
 import express from 'express';
 import { resolve } from 'path';
+import * as Sentry from '@sentry/node';
 
+import sentryConfig from './config/sentry';
 import routes from './routes';
 
+import 'express-async-errors';
 import './database';
 
 class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -23,6 +29,7 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
   }
 }
 
---
+++ Backend/src/config/sentry.js
@@ -0,0 +1,4 @@
+export default {
+  dsn:
+    'https://04d0656606f44f78874039ac79bc3099@o386876.ingest.sentry.io/5297878',
+};
